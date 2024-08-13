71c822a41b216c39729564bdb10fa62eb0f5c8c7
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2024-08-13 19:57:05 +0530

    Update AuthenticationController.java

--- src/main/java/com/tucs216/lifeplus/auth/AuthenticationController.java
+++ src/main/java/com/tucs216/lifeplus/auth/AuthenticationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/auth/")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://3.219.241.79:80")
 public class AuthenticationController {
 
     private final AuthenticationService service;
