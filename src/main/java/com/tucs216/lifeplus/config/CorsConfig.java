3ddfacf6d243b9375d0aa19569ae2bfd5df9a0de
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2024-08-13 19:34:54 +0530

    Update CorsConfig.java

--- src/main/java/com/tucs216/lifeplus/config/CorsConfig.java
+++ src/main/java/com/tucs216/lifeplus/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://3.219.241.79:80")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
