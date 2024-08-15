7c6bef851a365ac4f4ed65fd89b67afecb2952e5
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2024-08-15 15:29:34 +0530

    Update CustomerPolicyController.java

--- src/main/java/com/tucs216/lifeplus/controller/CustomerPolicyController.java
+++ src/main/java/com/tucs216/lifeplus/controller/CustomerPolicyController.java
@@ -22,7 +22,7 @@ import com.tucs216.lifeplus.services.CustomerPolicyService;
 
 @RestController
 @RequestMapping("/customer-policies")
-@CrossOrigin(origins = "http://34.234.35.144")
+@CrossOrigin(origins = "http://34.234.35.144:80")
 public class CustomerPolicyController {
     @Autowired
     private CustomerPolicyService customerPolicyService;
