788d8a416ee39aec859686548c42fc8a05d656ec
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2024-08-15 15:47:23 +0530

    Update PolicyController.java

--- src/main/java/com/tucs216/lifeplus/controller/PolicyController.java
+++ src/main/java/com/tucs216/lifeplus/controller/PolicyController.java
@@ -19,7 +19,7 @@ import com.tucs216.lifeplus.services.PolicyService;
 
 @RestController
 @RequestMapping("/policies")
-@CrossOrigin(origins = "http://34.234.35.144:80")
+@CrossOrigin(origins = "*")
 public class PolicyController {
     @Autowired
     private PolicyService policyService;
